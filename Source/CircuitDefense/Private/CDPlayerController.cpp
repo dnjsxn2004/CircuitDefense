47cb76c8c6b72691cc0b4868f5a3be62ee4069f9
dnjsxn2004 <dnjsxn2004@gmail.com>
2026-08-20 11:06:27 +0900

    feat: add game mode game state and player controller

---
+++ Source/CircuitDefense/Private/CDPlayerController.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CDPlayerController.h"
+
+ACDPlayerController::ACDPlayerController()
+{
+	bShowMouseCursor = true;
+	bEnableClickEvents = true;
+	bEnableMouseOverEvents = true;
+}
